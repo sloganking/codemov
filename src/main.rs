bcb145b6e16f7d75cc1f00f614bd65869b912190
Logan King <seanloganking@gmail.com>
2022-09-10 15:11:44 -0400

    resize images

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,74 @@
+use image::{DynamicImage, GenericImageView, RgbaImage};
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
+use glob::{glob, GlobError};
+
+/// Adds invisible padding around an image so it becomes the
+/// requested resolution. The new image will be in the center
+/// of the padding.
+fn add_buffer_till_image_is(x: u32, y: u32, old_img: &DynamicImage) -> DynamicImage {
+    if old_img.width() == x && old_img.height() == y {
+        return old_img.clone();
+    }
+
+    let mut new_img = RgbaImage::new(x, y);
+
+    // assert new dimentions are same or bigger.
+    if old_img.width() > x {
+        panic!();
+    }
+    if old_img.height() > y {
+        panic!();
+    }
+
+    let new_x0 = (x - old_img.width()) / 2;
+    let new_y0 = (y - old_img.height()) / 2;
+
+    for tx in 0..old_img.width() {
+        for ty in 0..old_img.height() {
+            new_img.put_pixel(new_x0 + tx, new_y0 + ty, old_img.get_pixel(tx, ty));
+        }
+    }
+
+    new_img.into()
+}
+
+fn resize_image_at(path: &str, x: u32, y: u32) {
+    let img = image::open(path).unwrap();
+    let img = img.resize(x, y, image::imageops::FilterType::Nearest);
+    let img = add_buffer_till_image_is(x, y, &img);
+
+    img.save(path).unwrap();
+}
+
+/// Returns a list of all files in a directory and it's subdirectories
+pub fn get_files_in_dir(path: &str, filetype: &str) -> Result<Vec<PathBuf>, GlobError> {
+    //> get list of all files and dirs in path, using glob
+        let mut paths = Vec::new();
+
+        let mut potential_slash = "";
+        if PathBuf::from(path).is_dir() && !path.ends_with('/') {
+            potential_slash = "/";
+        }
+
+        let search_params = String::from(path) + potential_slash + "**/*" + filetype;
+
+        for entry in glob(&search_params).expect("Failed to read glob pattern") {
+            match entry {
+                Ok(path) => {
+                    paths.push(path);
+                }
+                Err(e) => return Err(e),
+            }
+        }
+
+    //< 
+    // filter out directories
+    let paths = paths.into_iter().filter(|e| e.is_file()).collect();
+    
+    Ok(paths)
+}
 
 /// Erases all content of an existing directory, or creates an empty new one.
 pub fn clean_dir(dir: &str) {
@@ -59,6 +127,7 @@ fn main() {
 
     //<
 
+    // render frame for each commit
     for (i, commit) in commit_list.iter().rev().enumerate() {
         // git checkout <tag>
         println!("checking out: {}", commit);
@@ -69,12 +138,56 @@ fn main() {
 
         // create image
         let _ = Command::new("codevis")
-            .args(["-i", "./", "-o", &("../../frames/".to_owned() + &format!("{:09}", i) + ".png")])
+            .args([
+                "-i",
+                "./",
+                "-o",
+                &("../../frames/".to_owned() + &format!("{:09}", i) + ".png"),
+            ])
             .output()
             .unwrap();
     }
 
-    // clean_dir("./images/");
+    // move to frames directory
+    std::env::set_current_dir("../../frames/").expect("Unable to change directory");
+
+    //> resize all images
+
+        let paths = get_files_in_dir("./", "").unwrap();
+        println!("paths: {:?}", paths);
+        for path in paths{
+            resize_image_at(&path.into_os_string().into_string().unwrap(), 1920,1080);
+        }
+
+    //<> generating video
+
+        println!("generating video");
+
+        let out = Command::new("ls").output().unwrap();
+
+        println!("out: {:?}", out);
+
+        // create video
+        // ffmpeg -y -r 30 -f image2 -pattern_type glob -i '*.png' output.mp4
+        let out = Command::new("ffmpeg")
+            .args([
+                "-y",
+                "-r",
+                "30",
+                "-f",
+                "image2",
+                "-pattern_type",
+                "glob",
+                "-i",
+                "'*.png'",
+                "../output.mp4",
+            ])
+            .output()
+            .unwrap();
+
+        println!("out: {:?}", out);
+
+    //<
 
     // let out = Command::new("cd").args(["codevis/"]).output().unwrap();
 
